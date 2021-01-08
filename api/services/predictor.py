966fb7f1c0248c789b746e7f3cc1a4ec71462222
KamalShrest <kamal.shrestha@student.ku.edu.np>
2021-01-08 15:17:06 +0545

    [Refactor]: Add gitignore file

--- api/services/predictor.py
+++ api/services/predictor.py
@@ -22,31 +22,3 @@ def predict():
     print("[INFO: (api/service/predictor)]: User feedback received")
     prediction = sentence_prediction(payload)
     return render_template("predict.html", input_text=payload, output=prediction)
-
-
-def data():
-
-    # POST a data to database
-    if request.method == "POST":
-        body = request.json
-        name = body["name"]
-        age = body["age"]
-
-        # db.users.insert_one({
-        db["users"].insert_one({"name": name, "age": age})
-        return jsonify(
-            {"status": "Data is posted to MongoDB!", "name": name, "age": age}
-        )
-
-    # GET all data from database
-    if request.method == "GET":
-        allData = db["users"].find()
-        dataJson = []
-        for data in allData:
-            id = data["_id"]
-            name = data["name"]
-            age = data["age"]
-            dataDict = {"id": str(id), "name": name, "age": age}
-            dataJson.append(dataDict)
-        print(dataJson)
-        return jsonify(dataJson)
--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,4 @@ from api.services import predictor
 
 def app_routes(app):
     app.add_url_rule(rule="/", view_func=predictor.home, methods=["GET"])
-    app.add_url_rule(rule="/data", view_func=predictor.data, methods=["POST", "GET"])
     app.add_url_rule(rule="/predict", view_func=predictor.predict, methods=["POST"])
